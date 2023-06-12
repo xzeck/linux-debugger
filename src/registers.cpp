ff0095bc0bcbeaf4fd9434eb559a911f62b3a5c7
ajay <ajaypnair99@gmail.com>
2023-06-13 00:45:50 +0530

    Added required header files, corrected variable name errors, changed std::find to std::find_if and changed CMakeLists.txt to include registers.cpp
    Signed-off-by: ajay <ajaypnair99@gmail.com>

--- src/registers.cpp
+++ src/registers.cpp
@@ -1,11 +1,15 @@
 #include "registers.hpp"
+#include <sys/user.h>
+#include <sys/ptrace.h>
+#include <algorithm>
 
 uint64_t get_register_value(pid_t pid, reg r)
 {
     user_regs_struct regs;
     ptrace(PTRACE_GETREGS, pid, nullptr, &regs);
 
-    auto it = std::find(begin(g_register_descriptors), end(g_register_descriptors), [r](auto&& rd) { return rd.r == r; });
+    auto it = std::find_if(begin(g_register_descriptors), end(g_register_descriptors), 
+                        [r](auto&& rd) { return rd.r == r; });
 
-    return *(reinterpret_cast<uint64_t*>(&args) + (it - begin(g_register_descriptors)));
+    return *(reinterpret_cast<uint64_t*>(&regs) + (it - begin(g_register_descriptors)));
 }
